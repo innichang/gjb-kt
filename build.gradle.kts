d329a4695ae1596e85072c154ed4551e6295cbd3
Inseok Chang <inni.chang95@gmail.com>
2024-04-15 16:33:17 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.23"
+	kotlin("plugin.spring") version "1.9.23"
+	kotlin("plugin.jpa") version "1.9.23"
+	id("org.flywaydb.flyway") version "9.10.0"
+}
+
+group = "com.api"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-mysql")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+val localDbUrl = "jdbc:mysql://localhost:3306/goodjobbuddyKT"
+
+flyway {
+	url = localDbUrl
+	encoding = "UTF-8"
+	cleanDisabled = false
+	user = "root"
+	password = "950922"
+}
+
+
---
+++ src/main/kotlin/com/api/goodjobbuddy/kt/GoodJobBuddyKtApplication.kt
@@ -0,0 +1,11 @@
+package com.api.goodjobbuddy.kt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoodJobBuddyKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoodJobBuddyKtApplication>(*args)
+}
