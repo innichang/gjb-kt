51f694d61bb99bd4e99e26235f354a27a8ac5ee5
Inseok Chang <inni.chang95@gmail.com>
2024-04-15 20:02:12 +0900

    chore: QueryDsl 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.23"
 	kotlin("plugin.jpa") version "1.9.23"
 	id("org.flywaydb.flyway") version "9.10.0"
+	kotlin("kapt") version "1.9.21"  //Kotlin annotationProcessor plugin
 }
 
 group = "com.api"
@@ -15,10 +16,17 @@ version = "0.0.1-SNAPSHOT"
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
 }
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
 
 repositories {
 	mavenCentral()
 }
+
+val queryDslVersion: String by extra
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -30,6 +38,19 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// jwt
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	// QueryDSL 의존성 추가
+	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+	kapt("jakarta.annotation:jakarta.annotation-api")
+	kapt("jakarta.persistence:jakarta.persistence-api")
+	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.8.0") // jpa 파라미터 매칭 확인 가능 라이브러리
 }
 
 tasks.withType<KotlinCompile> {
@@ -52,5 +73,6 @@ flyway {
 	user = "root"
 	password = "950922"
 }
+
 
 
